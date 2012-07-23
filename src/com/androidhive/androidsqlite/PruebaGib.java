158f2b264a7cc178fd45dd001792df5a27f91d38
Miguel Angel Garrido <miguelgarridogtz@gmail.com>
2012-07-23 12:30:20 -0500

    aCEPTA PNDJO

---
+++ src/com/androidhive/androidsqlite/PruebaGib.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.androidhive.androidsqlite;
+
+/**
+ *
+ * @author CPP-lap
+ */
+public class PruebaGib {
+    public void SayHello(){
+        System.out.print("Hola");
+    }
+    
+}
